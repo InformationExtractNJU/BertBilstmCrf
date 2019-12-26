541cea4df889d95255efd0614121da834b31256f
jiaohuazhiyichong <112987845@qq.com>
2019-12-26 17:23:35 +0800

    更新了相关结果的统计

--- BertBilstmCrfKfold.py
+++ BertBilstmCrfKfold.py
@@ -56,6 +56,7 @@ n_tags = len(tag2idx)
 print(tag2idx)
 
 import re
+# 获取训练数据
 def get_train_data():
     train_data = []
     n2id = []
@@ -81,10 +82,10 @@ def get_train_data():
             count = count+1
     return train_data
 
+# 加载训练数据
 train_data = get_train_data()
 print('数据读取完毕')
 print(len(train_data[0]))
-
 print(train_data[0])
 
 
@@ -101,15 +102,8 @@ model = keras.models.Model(inputs=[x1_in,x2_in],outputs=out)
 model.compile(loss=crf.loss_function,optimizer='adam',metrics=[crf.accuracy])
 model.summary()
 iteration_count = 1
-save_path = 'model'
-filepath="model_{epoch:02d}-{val_crf_viterbi_accuracy:.4f}.hdf5"
 
-callbacks = [
-    keras.callbacks.EarlyStopping(monitor='val_loss',patience=2,verbose=0),
-    keras.callbacks.ModelCheckpoint(os.path.join(save_path,filepath),
-monitor='val_loss',save_best_only=True, verbose= 0),
-]
-
+# 采用5折交叉验证
 kf = KFold(n_splits=5)
 id_,text_id,X1,X2,Y=[],[],[],[],[]
 f1_news_score = []
@@ -118,8 +112,32 @@ precision_news_score = []
 iteration_count = 1
 
 result_report = []
-
+# 定义获取实体的函数
+def get_entity(X_data, y_data,B_tag,I_tag):
+    entity_list = []
+    entity_name = ''
+    for i,(c,l) in enumerate(zip(X_data,y_data)):
+        if l == B_tag:
+            entity_name += c
+        elif (l == I_tag) and (len(entity_name)) > 0:
+            entity_name += c
+            if i == len(y_data)-1:
+                entity_list.append(entity_name)
+        elif l == 'O':
+            if(len(entity_name)) > 0:
+                entity_list.append(entity_name)
+            entity_name = ''
+    return ';'.join(list(set(entity_list)))
+# 采用五折交叉验证，获取数据来进行训练
 for train,test in kf.split(range(len(train_data[1:61]))):
+    save_path = 'model/model'
+    save_path += str(iteration_count)
+    filepath = "model_{epoch:02d}-{val_crf_viterbi_accuracy:.4f}.hdf5"
+    callbacks = [
+        keras.callbacks.EarlyStopping(monitor='val_loss', patience=2, verbose=0),
+        keras.callbacks.ModelCheckpoint(os.path.join(save_path, filepath),
+                                        monitor='val_loss', save_best_only=True, verbose=0),
+    ]
     print("这是第"+str(iteration_count)+'轮交叉验证')
     print('*'*100)
     print(test)
@@ -229,16 +247,30 @@ for train,test in kf.split(range(len(train_data[1:61]))):
         if t != "-PAD-":
             print("{:15}: {:5} {}".format(c, t, p))
     # 统计每轮验证的结果
-    pred_label_list = []
-    true_label_list = []
-    tid_list = []
-    text_list = []
+    pred_label_list = []  #代表预测标签系列
+    true_label_list = []   #代表真实标签序列
+    tid_list = []    #代表文章id序列, 加入结果集
+    text_list = []    #代表正文序列, 加入结果集
+    type_list = []    #代表类型，Manuslly代表手动标注的结果，Predict代表模型预测的结果, 加入结果集
+    tag_str_list = []  #代表预测序列, 加入结果集
+    car_list = []  #代表汽车序列, 加入结果集
+    company_list = [] #代表公司序列, 加入结果集
+    person_list = [] #代表人名序列, 加入结果集
+    technology_list = []    # 代表技术序列, 加入结果集
+    hardware_list = []    # 代表硬件序列, 加入结果集
+    software_list = []    # 代表软件序列, 加入结果集
+    patent_list = []    # 代表专利序列, 加入结果集
+    date_list = []    # 代表日期序列, 加入结果集
+    loc_list = []     # 代表地点序列, 加入结果集
+    service_list = []  # 代表服务序列, 加入结果集
     for i in range(len(id_test)):
         sample_X = X1_test[i]  #sample_X代表所获得的文本
-        tid = id_test[i][0]   #tid代表测试集的id
-        sample_text_id = text_id_test[i][0] #
+        sample_text_id = text_id_test[i][0]
+        print(sample_text_id)
         tid_list.append(sample_text_id)
-        sample_data = train_data[tid]
+        tid_list.append(sample_text_id)
+        sample_data = train_data[sample_text_id]
+        text_list.append(train_data[sample_text_id][1])
         text_list.append(train_data[sample_text_id][1])
         sample_Y = Y_test[i]
         predict = model.predict([sample_X.reshape([1, -1]),sample_X.reshape([1, -1])])
@@ -258,10 +290,77 @@ for train,test in kf.split(range(len(train_data[1:61]))):
                 true_str += ' '
         pred_label_list.append(pred_str)
         true_label_list.append(true_str)
-    dict_result = {'id': tid_list, '文本内容': text_list, '预测标签': pred_label_list, '真实标签': true_label_list}
+        tag_str_list.append(true_str)
+        tag_str_list.append(pred_str)
+        type_list.append('Manually')
+        type_list.append('Predict')
+        X_data = [c for c in sample_data[1]]
+
+        # 预测汽车
+        car_eneity_pre = get_entity(X_data,pred_label,'B-CAR','I-CAR')
+        car_eneity_true = get_entity(X_data,true_label,'B-CAR','I-CAR')
+        car_list.append(car_eneity_true)
+        car_list.append(car_eneity_pre)
+
+        # 预测公司
+        company_eneity_pre = get_entity(X_data,pred_label,'B-COMPANY','I-COMPANY')
+        company_eneity_true = get_entity(X_data,true_label,'B-COMPANY','I-COMPANY')
+        company_list.append(company_eneity_true)
+        company_list.append(company_eneity_pre)
+
+        # 预测人名
+        person_eneity_pre = get_entity(X_data,pred_label,'B-PER','I-PER')
+        person_eneity_true = get_entity(X_data,true_label,'B-PER','I-PER')
+        person_list.append(person_eneity_true)
+        person_list.append(person_eneity_pre)
+
+        # 预测技术
+        technology_eneity_pre = get_entity(X_data,pred_label,'B-TEL','I-TEL')
+        technology_eneity_true = get_entity(X_data,true_label,'B-TEL','I-TEL')
+        technology_list.append(technology_eneity_true)
+        technology_list.append(technology_eneity_pre)
+
+        # 预测硬件
+        hardware_eneity_pre = get_entity(X_data,pred_label,'B-HARDWARE','I-HARDWARE')
+        hardware_eneity_true = get_entity(X_data,true_label,'B-HARDWARE','I-HARDWARE')
+        hardware_list.append(hardware_eneity_true)
+        hardware_list.append(hardware_eneity_pre)
+
+        # 预测软件
+        software_eneity_pre = get_entity(X_data, pred_label, 'B-SOFTWARE', 'I-SOFTWARE')
+        software_eneity_true = get_entity(X_data, true_label, 'B-SOFTWARE', 'I-SOFTWARE')
+        software_list.append(software_eneity_true)
+        software_list.append(software_eneity_pre)
+
+        # 预测服务
+        service_eneity_pre = get_entity(X_data, pred_label, 'B-SERVICE', 'I-SERVICE')
+        service_eneity_true = get_entity(X_data, true_label, 'B-SERVICE', 'I-SERVICE')
+        service_list.append(service_eneity_true)
+        service_list.append(service_eneity_pre)
+
+        # 预测专利
+        patent_eneity_pre = get_entity(X_data, pred_label, 'B-PATENT', 'I-PATENT')
+        patent_eneity_true = get_entity(X_data, true_label, 'B-PATENT', 'I-PATENT')
+        patent_list.append(patent_eneity_true)
+        patent_list.append(patent_eneity_pre)
+
+        # 预测时间
+        date_eneity_pre = get_entity(X_data, pred_label, 'B-TIME', 'I-TIME')
+        date_eneity_true = get_entity(X_data, true_label, 'B-TIME', 'I-TIME')
+        date_list.append(date_eneity_true)
+        date_list.append(date_eneity_pre)
+
+        # 预测地点
+        loc_eneity_pre = get_entity(X_data, pred_label, 'B-LOC', 'I-LOC')
+        loc_eneity_true = get_entity(X_data, true_label, 'B-LOC', 'I-LOC')
+        loc_list.append(loc_eneity_true)
+        loc_list.append(loc_eneity_pre)
+    dict_result = {'id': tid_list, 'Text': text_list, 'Type':type_list, 'Company':company_list,
+                   'Car':car_list,'Person':person_list,'Technique':technology_list,'Hardware':hardware_list,
+                   'Software':software_list,'Patent':patent_list,'Date':date_list,'Loc':loc_list ,'SequenceResult':tag_str_list }
     pd_result = pd.DataFrame(dict_result)
     print(pd_result.head())
-    pd_result.to_excel('Test_case/Fold' + str(iteration_count - 1) + 'Result.xls', encoding='utf-8')
+    pd_result.to_excel('Test_case/Fold' + str(iteration_count - 1) + 'Result.xls', index = False,encoding='utf-8')
 
 print('平均f1值')
 print(np.array(f1_news_score).mean())
